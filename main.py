99a19d38c5132aa15adcb03e51ea976658d815d0
Mdbook <mikayla@mdbook.me>
2023-08-22 14:48:49 -0400

    Server running

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return "<p>Welcome to KaraQueue!</p>"
+app.run(host="0.0.0.0")
