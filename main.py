46af8e6cae6b0dcd756c39dc19f6931b13e45e64
Mdbook <mikayla@mdbook.me>
2023-08-22 16:59:09 -0400

    Basic login done

---
+++ main.py
@@ -0,0 +1,76 @@
+
+from flask import url_for, render_template, request, redirect, session
+
+# from instagram import get_followed_by, get_user_name
+from app import app
+from users import User, db, test_admin
+
+    # default_admin = User(
+    #     username="admin",
+    #     password="admin")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    """ Session control"""
+    if not session.get('logged_in'):
+        return render_template('index.html')
+    else:
+        if request.method == 'POST':
+            username = get_user_name(request.form['username'])
+            return render_template('index.html', data=get_followed_by(username))
+        return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Login Form"""
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        name = request.form['username']
+        passw = request.form['password']
+        try:
+            data = User.query.filter_by(username=name, password=passw).first()
+            if data is not None:
+                session['logged_in'] = True
+                session['admin'] = data.admin
+                session['username'] = data.username
+                return redirect(url_for('home'))
+            else:
+                return 'Dont Login'
+        except:
+            return "Dont Login"
+
+
+@app.route('/register/', methods=['GET', 'POST'])
+def register():
+    """Register Form"""
+    if request.method == 'POST':
+        data = User.query.filter_by(username=request.form['username']).first()
+        if data is not None:
+            return render_template('register.html', userExists=True)
+        else:
+            new_user = User(
+                username=request.form['username'],
+                password=request.form['password'])
+            db.session.add(new_user)
+            db.session.commit()
+            return render_template('login.html')
+    return render_template('register.html')
+
+
+@app.route("/logout")
+def logout():
+    """Logout Form"""
+    session['logged_in'] = False
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.app_context().push()
+    app.debug = True
+    db.create_all()
+    test_admin()
+    app.secret_key = "123"
+    app.run(host='0.0.0.0')
+
---
+++ users.py
@@ -0,0 +1,31 @@
+
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+db = SQLAlchemy(app)
+class User(db.Model):
+    """ Create user table"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(80))
+    admin = db.Column(db.Boolean, unique=False,default=False)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.admin = False
+
+def test_admin():
+    with app.app_context():
+        data = User.query.filter_by(username="admin").first()
+        if data is None:
+            default_admin = User(
+                username="admin",
+                password="admin")
+            db.session.add(default_admin)
+            db.session.commit()
+            data = User.query.filter_by(username="admin").first()
+            data.admin = True
+            db.session.commit()
+            print("Created default admin user account")
+        else:
+            print(data.admin)
